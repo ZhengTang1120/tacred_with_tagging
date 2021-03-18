27d08ff32c30f78a4c4aff3f7d163ed0fa3b491e
Zheng Tang <oscartang1120@gmail.com>
2021-03-17 22:17:10 -0700

    Update train.py

--- train.py
+++ train.py
@@ -7,4 +7,5 @@ tokenizer = BertTokenizer.from_pretrained('mrm8488/spanbert-large-finetuned-tacr
 special_tokens_dict = {'additional_special_tokens': constant.ENTITY_TOKENS}
 num_added_toks = tokenizer.add_special_tokens(special_tokens_dict)
 
-train = DataLoader(opt['data_dir'] + '/train.json', opt['data_dir'] + '/interval_train.txt', opt['data_dir'] + '/pattern_train.txt', tokenizer)
+data_dir = 'tacred'
+train = DataLoader(data_dir + '/train.json', data_dir + '/interval_train.txt', data_dir + '/pattern_train.txt', tokenizer)
